b35ae56fa173b43365936b3ac2d3b24ee08b6fd2
hemangandhi <hemangandhi@gmail.com>
2015-04-17 18:21:26 -0400

    Did euler problem 7

--- online-problems/src/online_problems/euler-6.clj
+++ online-problems/src/online_problems/euler-6.clj
@@ -3,3 +3,15 @@
 (defn euler-6 [x] (- (#(* % %) (apply + (range x))) 
   (apply + (map #(* % %) (range x)))))
 
+(defn all-+? [list] (if (empty? list) true (and (< 0 (first list)) (all-+? (rest list)))))
+
+(defn n-primes ([n] (n-primes [2] n))
+ ([primes n] (if (= 0 n) 
+               primes 
+               (n-primes (conj primes 
+                         (let [np-a (atom (+ 1 (last primes)))]
+                           (while (not= 0 (count (filter #(= 0 (mod @np-a %)) primes))) 
+                             (do (swap! np-a inc)))
+                          @np-a)) 
+                         (- n 1)))))
+                
