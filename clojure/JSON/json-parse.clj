476e396f3ffc1578dfd628bc76ef12c535c45c4e
hemangandhi <hemangandhi@gmail.com>
2016-02-07 22:34:19 -0500

    improved clj json parser

--- clojure/JSON/json-parse.clj
+++ clojure/JSON/json-parse.clj
@@ -1,34 +1,29 @@
 (ns json-parse.core)
 
-(defn string-split [st va ig-st ig-end ig-same]
-  (loop [i 0 ins 0 lst-sub -1 lst '() init-ins 0]
-    (if (= i (count st))
-      (reverse (conj lst (.substring st (inc lst-sub) i)))
-      (if (and (.contains va (.charAt st i)) (= 0 ins))
-        (recur (inc i) ins i (conj lst (.substring st (inc lst-sub) i)) init-ins)
-        (if (.contains ig-st (.charAt st i))
-          (recur (inc i) (inc ins) lst-sub lst (if (= 0 init-ins) 1 0))
-          (if (.contains ig-end (.charAt st i))
-            (recur (inc i) (if (= ins 0) 0 (dec ins)) lst-sub lst (if (= 0 init-ins) 1 0))
-            (if (.contains ig-same (.charAt st i))
-              (recur (inc i) (if (= 0 (mod (+ init-ins ins) 2)) (inc ins) (dec ins)) lst-sub lst init-ins)
-              (recur (inc i) ins lst-sub lst init-ins))))))))
+(defn string-split [st va ig-map]
+  (loop [i 0 p '() r [-1]]
+    (cond
+      (>= i (count st)) (let [nr (conj r (count st))]
+                          (filter #(< 0 (count %)) 
+                                  (for [j (range 1 (count nr))] 
+                                    (subs st (inc (nth nr (dec j))) (nth nr j)))))
+      (and (empty? p) (.contains va (nth st i))) (recur (inc i) p (conj r i))
+      (and (.contains (vals ig-map) (nth st i)) 
+           (= (nth st i) (ig-map (first p)))) (recur (inc i) (rest p) r)
+      (contains? ig-map (nth st i)) (recur (inc i) (conj p (nth st i)) r)
+      :else (recur (inc i) p r))))
 
-(defn obj-to-vec [obj]
-  (into [] (string-split obj [\: \,] [\[ \{] [\] \}] [\"])))
+(defn json-obj-to-map [obj-str]
+  (apply hash-map (string-split obj-str #{\: \, \space} {\' \', \" \", \[ \], \{ \}})))
 
-(defn arr-to-vec [arr]
-  (into [] (string-split arr [\,] [\[ \{] [\] \}] [\"])))
+(defn json-arr-to-vec [arr-str]
+  (vec (string-split arr-str #{, \space} {\' \', \" \", \[ \], \{ \}})))
 
-(defn full-json-parse [json]
-  (if (= (first json) \[)
-    (map #(full-json-parse (.trim %)) (arr-to-vec (.substring json 1 (dec (count json)))))
-    (if (= (first json) \{)
-      (apply hash-map (map #(full-json-parse (.trim %)) (obj-to-vec (.substring json 1 (dec (count json))))))
-      (read-string (.trim json)))))
+(defn full-parse [st]
+  (condp = (first st)
+    \[ (map full-parse (json-arr-to-vec (subs st 1 (dec (count st)))))
+    \{ (let [mp (json-obj-to-map (subs st 1 (dec (count st))))]
+         (into {} (for [[k v] mp] [(full-parse k) (full-parse v)])))
+    (read-string st)))
 
-(defn parse-file [path]
-  (full-json-parse (.trim (.replaceAll (slurp path) "[\n\r]" ""))))
-
-;(get (first (parse-file "C:\\Users\\Heman\\Documents\\Torchlight\\web\\events.json")) "time")
-
+(println (full-parse "{\"name\" : \"Price, Elder\", \"age\": 39, \"wives\": [1, 2, 3]}"))
