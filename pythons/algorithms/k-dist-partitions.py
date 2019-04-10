0890d8cfea8047fb243f0bbc8bc1e6137bb89cf3
Heman Gandhi <hemangandhi@gmail.com>
2019-04-10 11:28:25 -0400

    tuples can be hashed uwu

--- pythons/algorithms/k-dist-partitions.py
+++ pythons/algorithms/k-dist-partitions.py
@@ -4,16 +4,21 @@ def k_dist_perms(ls, k):
     if len(ls) <= k:
         yield from permutations(ls)
     else:
-        for i in range(k + 1):
+        for i in range(1, k + 1):
             lhss = list(permutations(ls[:i]))
             for rhs in k_dist_perms(ls[i:], k):
                 yield from (lhs + rhs for lhs in lhss)
+
+k_dist_perms_dedup = lambda ls, k: set(k_dist_perms(ls, k))
 
 def k_dist_perm_checker(init, k):
     def is_k_dist(perm):
         return all(p in init[i - k: i + k + 1] for i, p in enumerate(perm))
 
 if __name__ == "__main__":
-    test_list, test_k = [], 0
+    test_list, test_k = [1, 2, 3], 2
     naive = list(filter(k_dist_perm_checker(test_list, test_k), permutations(test_list)))
-    print(list(k_dist_perms(test_list, test_k)) == naive)
+    print(naive)
+    non_naive = list(k_dist_perms_dedup(test_list, test_k))
+    print(non_naive)
+    print(non_naive == naive)
