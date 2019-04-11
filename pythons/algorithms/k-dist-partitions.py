74c38580e233fc6330af0aa2d4f4805381b6d622
Heman Gandhi <hemangandhi@gmail.com>
2019-04-11 13:56:49 -0400

    bug fixes, but not enough

--- pythons/algorithms/k-dist-partitions.py
+++ pythons/algorithms/k-dist-partitions.py
@@ -2,18 +2,25 @@ from itertools import permutations
 
 def dedup(fn):
     def wrapper(ls, k):
-        return set(fn(ls, k))
+        i = fn(ls, k)
+        # i = list(i)
+        rv = set(i)
+        # print(len(i), len(rv))
+        return rv
     return wrapper
 
+#DOESN'T WORK AND I DON'T KNOW WHY
 @dedup
 def k_dist_perms_part(ls, k):
-    if len(ls) <= k:
-        yield from permutations(ls)
-    else:
-        for i in range(1, k + 1):
-            lhss = list(permutations(ls[:i]))
-            for rhs in k_dist_perms(ls[i:], k):
-                yield from (lhs + rhs for lhs in lhss)
+    def rec_helper(ls, k): #avoids deduping until the very end
+        if len(ls) <= k:
+            yield from permutations(ls)
+        else:
+            for i in range(1, k + 2):
+                lhss = list(permutations(ls[:i]))
+                for rhs in k_dist_perms_part(ls[i:], k):
+                    yield from (lhs + rhs for lhs in lhss)
+    return rec_helper(ls, k)
 
 @dedup
 def k_dist_perms_build(ls, k):
@@ -25,7 +32,7 @@ def k_dist_perms_build(ls, k):
                 if j not in built:
                     cp = built.copy()
                     cp[j] = i
-                    yield from build_helper(i + 1, built)
+                    yield from build_helper(i + 1, cp)
 
     #conjecture: all k-distance permutations are the inverse of some k-distance permutation
     def built_to_perm(built):
@@ -35,13 +42,13 @@ def k_dist_perms_build(ls, k):
 
 def k_dist_perm_checker(init, k):
     def is_k_dist(perm):
-        return all(p in perm[i - k: i + k + 1] for i, p in enumerate(init))
+        return all(p in perm[max(i - k, 0): min(i + k + 1, len(init))]\
+                for i, p in enumerate(init))
     return is_k_dist
 
 if __name__ == "__main__":
-    test_list, test_k = [1, 2, 3], 2
-    naive = list(filter(k_dist_perm_checker(test_list, test_k), permutations(test_list)))
-    print(naive)
-    non_naive = list(k_dist_perms(test_list, test_k))
-    print(non_naive)
+    test_list, test_k = list(range(6)), 4
+    naive = set(filter(k_dist_perm_checker(test_list, test_k), permutations(test_list)))
+    non_naive = k_dist_perms_part(test_list, test_k)
+    print(naive - non_naive)
     print(non_naive == naive)
