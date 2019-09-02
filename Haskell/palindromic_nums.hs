9be8afd71140da5ec6f4b185a5a74e30f6a8c3df
Heman Gandhi <hemangandhi@gmail.com>
2019-09-02 17:34:37 -0400

    palindrome experiments

---
+++ Haskell/palindromic_nums.hs
@@ -0,0 +1,10 @@
+
+is_palindrome x = x == (reverse x)
+
+encode_in_base b x = (x, reverse $ encode_loop b x)
+    where encode_loop base 0 = ""
+          encode_loop base x = (show $ x `mod` base) ++ (encode_loop base (x `quot` base))
+
+palindromes_in_base b = filter (is_palindrome . snd) $ map (encode_in_base b) [1..]
+
+seq_of_diffs s = zipWith (-) (map fst $ drop 1 s) $ map fst s
