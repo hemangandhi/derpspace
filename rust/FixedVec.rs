8051f1fb83e06f0042482a6571752186ccd13b18
Heman Gandhi <hemangandhi@gmail.com>
2020-04-26 12:52:57 -0400

    Rust GADT 6(?): fewer lifetime troubles

--- rust/FixedVec.rs
+++ rust/FixedVec.rs
@@ -33,39 +33,60 @@ type Plus<X: Nat, Y: Nat> = <X as AddNats<Y>>::Sum;
 
 // A list that knows its length as a type.
 pub trait FixedList<A, L: Nat> {
-    fn reify(&self) -> Vec<&A>;
+    fn reify(&self) -> Vec<A>;
+    fn box_clone(&self) -> Box<dyn FixedList<A, L>>;
 }
 
-pub struct EmptyList<A>(PhantomData<A>); /* surpress unused warning */
-impl<A> FixedList<A, ZNat> for EmptyList<A> {
-    fn reify(&self) -> Vec<&A> { vec![] }
-}
-
-pub struct ConsList<A, Lm1: Nat>(A, Box<dyn FixedList<A, Lm1>>);
-impl<A, Lm1: Nat> FixedList<A, SNat<Lm1>> for ConsList<A, Lm1> {
-    fn reify(&self) -> Vec<&A> {
-	let Self(curr, rest) = self;
-	let mut v = rest.reify();
-	v.insert(0, curr);
-	return v;
+impl<A, L: Nat> Clone for Box<dyn FixedList<A, L>> {
+    fn clone(&self) -> Self {
+	(*self).box_clone()
     }
 }
 
-// TODO(to make this build): need an alterate bound to account for the fact that a ConsList<A, n> is a FixedList<A, succ n>.
+pub struct EmptyList<A>(PhantomData<A>); /* surpress unused warning */
+
+impl<A: Clone> FixedList<A, ZNat> for EmptyList<A> {
+    fn reify(&self) -> Vec<A> { vec![] }
+    fn box_clone(&self) -> Box<dyn FixedList<A, ZNat>> {
+	let Self(ph) = *self;
+	let rv = Box::new(Self(ph.clone()));
+	rv
+    }
+}
+
+pub struct ConsList<A: Clone, Lm1: 'static + Nat>(A, Box<dyn FixedList<A, Lm1>>);
+
+impl<A: Clone, Lm1: 'static + Nat> FixedList<A, SNat<Lm1>> for ConsList<A, Lm1> {
+    fn reify(&self) -> Vec<A> {
+	let Self(curr, rest) = self;
+	let mut v = rest.reify();
+	v.insert(0, curr.clone());
+	return v;
+    }
+    fn box_clone(&self) -> Box<dyn FixedList<A, SNat<Lm1>>> {
+	let Self(x, xs) = *self;
+        let rv = Box::new(Self(x.clone(), (*xs).box_clone()));
+	rv
+    }
+}
+
 pub trait ConcatList<A, L1: Nat + AddNats<L2>, L2: Nat + AddNats<L1>, X: FixedList<A, L1>>: FixedList<A, L2>{
     fn concat_lists(self, X) -> Box<dyn FixedList<A, Plus<L2, L1>>>;
 }
 impl<A, L1, X> ConcatList<A, L1, ZNat, X> for EmptyList<A>
-where L1: Nat + AddNats<ZNat>,
+where A:Clone,
+      L1: Nat + AddNats<ZNat>,
       X: FixedList<A, L1> {
-    fn concat_lists(self, x: X) -> Box<dyn FixedList<A, Plus<ZNat, L1>>> { Box::new(x) }
+    fn concat_lists(self, x: X) -> Box<dyn FixedList<A, Plus<ZNat, L1>>> { x.box_clone() }
 }
 impl<A, L1, L2m1, X> ConcatList<A, L1, SNat<L2m1>, X> for ConsList<A, L2m1>
-where L1: Nat + AddNats<SNat<L2m1>>, L2m1: Nat + AddNats<L1>,
+where A:Clone,
+      L1: Nat + AddNats<SNat<L2m1>> + AddNats<L2m1>,
+      L2m1: Nat + AddNats<L1>,
       X: FixedList<A, L1> {
     fn concat_lists(self, l: X) -> Box<dyn FixedList<A, Plus<SNat<L2m1>, L1>>> {
 	let Self(x, xs) = self;
-	Box::new(ConsList(x, xs.concat_lists(l)))
+	Box::new(ConsList(x, (*xs).concat_lists(l)))
     }
 }
 
