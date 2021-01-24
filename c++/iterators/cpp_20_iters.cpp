2661fcb2404edc3344e4aacd75ece4620ecbd495
hemangandhi <hemangandhi@gmail.com>
2021-01-24 17:29:23 -0500

    better iterator concepts

--- c++/iterators/cpp_20_iters.cpp
+++ c++/iterators/cpp_20_iters.cpp
@@ -1,17 +1,11 @@
 #include <coroutine>
+#include <concepts>
 #include <cstddef> // size_t
 #include <cmath> // pow
 
-// A quick concept for what we think containers are.
-template<typename C, typename T>
-concept bool Container = requires(C a, std::size_t b) {
-  typename C<T>; // We need to have the container of T,
-  { a.size(); } -> std::size_t; // with a size,
-  { a[b]; } -> const T&; // and element access.
-};
-
-template<typename T, Container<T> Cont>
-std::generator<T> get_subset_from(const Cont<T>& cont, unsigned int flags) {
+template <std::input_iterator I>
+generator<typename std::iterator_traits<I>::value_type>
+get_subset_from(const I& cont, unsigned int flags) {
   for (std::size_t i = 0; i < cont.size(); i++) {
     if ((flags >> i) & 1) {
       co_yield cont[i];
@@ -19,8 +13,9 @@ std::generator<T> get_subset_from(const Cont<T>& cont, unsigned int flags) {
   }
 }
 
-template<typename T, Container<T> Cont>
-std::generator<std::generator<T>> subsets_iter(const Cont<T>& cont) {
+template <std::input_iterator I>
+generator<generator<typename std::iterator_traits<I>::value_type>>
+subsets_iter(const I& cont) {
   std::size_t subsets {Math.pow(2.0, cont.size())};
   for(std::size_t i = 0; i < subsets; i++) {
     co_yield get_subset_from(cont, i);
