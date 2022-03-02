a2315c5809413f5e7a471ca0cbf71e4ccb844cb5
Heman Gandhi <hemangandhi@gmail.com>
2022-03-01 21:45:29 -0500

    Update Julia wordle solver: now somewhat complete

--- Julia/wordle.jl
+++ Julia/wordle.jl
@@ -1,26 +1,5 @@
 # TODO: replace 5 with a word limit param?
 
-module sum_types
-  struct Left{T}
-    val::T
-  end
-
-  struct Right{T}
-    val::T
-  end
-
-  const Sum{T, U} = Union{Left{T}, Right{U}}
-
-  left_or_right_is(s::Left{T}, v::T) where {T} = s == v
-  left_or_right_is(s::Right{T}, v::T) where {T} = s == v
-end
-
-function read_dictionary(path)
-  Base.open(path, "r") do file
-    Base.Set(filter(line -> length(line) == 5, Base.eachline(file)))
-  end
-end
-
 # Perhaps better called "map_reduce" lol
 # (c -> a -> c) -> [a] -> c -> (a -> b) -> Map b c
 function group_by(merger::Function, iter, initialize, key_proj = x -> x)
@@ -72,7 +51,11 @@ mutable struct WordInfo
   # If the bool is true, that number is exact. Else, it's a lower bound
   letter_map::Dict{Char, Tuple{Bool, Int8}}
   word::Array{Union{Nothing, Char}}
-  WordInfo() = new(Dict(), Array{Union{Nothing, Char}}(nothing, 5))
+  place_filter::Array{Set{Char}}
+  WordInfo() = new(
+    Dict(),
+    Array{Union{Nothing, Char}}(nothing, 5),
+    fill(Set(), 5))
 end
 
 struct WordInfoIncorrectCharError <: Exception
@@ -84,6 +67,9 @@ function update_info!(info, scores, guess)
   # put all the correctly spotted characters into the word we're building
   for i = 1:5
     if scores[i] != correct
+      if scores[i] == present
+        push!(info.place_filter[i], guess[i])
+      end
       continue
     end
     if info.word[i] == nothing
@@ -102,4 +88,54 @@ function update_info!(info, scores, guess)
     end
   end
   info.letter_map = merge(info.letter_map, guess_counts)
+end
+
+function info_allows_word(info, word)
+  for i = 1:5
+    if info.word[i] != nothing && info.word[i] != word[i]
+      return false
+    elseif info.place_filter[i] in word[i]
+      return false
+    end
+  end
+  word_map = get_letter_counts(word)
+  for (letter, count) in pairs(word_map)
+    strict, bound = get(info.letter_map, letter, (false, 0))
+    if strict && count != bound
+      return false
+    elseif count < bound
+      return false
+    end
+  end
+  true
+end
+
+# Entropy = sum(p(1 - p)). The idea being that you want to be as close to halving the pending
+# guesses as possible.
+# TODO: generalize this (and below)
+# For example: nerdle.
+function highest_entropy_word(info, guess_dictionary, solution_dictionary)
+  best_entropy = nothing
+  best_guess = nothing
+  for guess in guess_dictionary
+    entropy = 0
+    for solution in solution_dictionary
+      info_copy = deepcopy(info)
+      update_info!(info_copy, score_wordle_guess(guess, solution), guess)
+      still_feasible = length(filter(w -> info_allows_word(info_copy, w), solution_dictionary))
+      # We can look at p(1 - p) as this formula after factoring out the denominator.
+      entropy += still_feasible * (length(solution_dictionary) - still_feasible)
+    end
+    if best_entropy == nothing || best_entropy < entropy
+      best_entropy = entropy
+      best_guess = guess
+    end
+  end
+  best_guess
+end
+
+function read_dictionary(path)
+  Base.open(path, "r") do file
+    Base.Set(Base.eachline(file))
+  end
 end
