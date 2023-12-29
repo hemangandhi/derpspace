cb6b45ee41e2bc831914ad9465f976c3d5826e68
Heman Gandhi <hemangandhi@gmail.com>
2023-12-30 00:46:07 +0900

    Add greedy strategies

--- rust/coding-game-2023-12-event/main.rs
+++ rust/coding-game-2023-12-event/main.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::convert::TryInto;
 use std::io;
 
 macro_rules! parse_input {
@@ -9,6 +10,7 @@ macro_rules! parse_input {
 
 #[derive(Clone, Copy)]
 enum Instruction {
+    // TODO: s/u32/i32/
     Move(u32, u32, bool),
     Wait(bool),
 }
@@ -22,10 +24,45 @@ impl Instruction {
         }
     }
 
-    fn with_big_light(self, big_light: bool) -> Self {
+    fn is_large_scan(&self) -> bool {
+        match *self {
+            Instruction::Move(x, y, l) => l,
+            Instruction::Wait(l) => l,
+        }
+    }
+
+    fn with_light(self, big_light: bool) -> Self {
         match self {
             Instruction::Move(x, y, _l) => Instruction::Move(x, y, big_light),
-            Instruction::Wait(_b) => Instruction::Wait(big_light)
+            Instruction::Wait(_b) => Instruction::Wait(big_light),
+        }
+    }
+
+    fn flip_light_for_drone_greedily(
+        self,
+        drone: &Drone,
+        creature_map: &HashMap<i32, Creature>,
+        me: &Player,
+        foe: &Player,
+    ) -> Self {
+        if drone.battery < 5 {
+            return self.with_light(false);
+        }
+        let new_point = drone.get_position_after_instruction(&self);
+        let dim_score = me.score_scan(
+            Drone::get_scan_from_point(new_point, creature_map, false),
+            creature_map,
+            foe,
+        );
+        let bright_score = me.score_scan(
+            Drone::get_scan_from_point(new_point, creature_map, true),
+            creature_map,
+            foe,
+        );
+        if (bright_score - dim_score) > (3 - drone.battery / 5) {
+            self.with_light(true)
+        } else {
+            self.with_light(false)
         }
     }
 }
@@ -50,14 +87,18 @@ struct Drone {
 }
 
 impl Drone {
-    fn get_scan(&self, creature_map: &HashMap<i32, Creature>, large: bool) -> HashSet<i32> {
+    fn get_scan_from_point(
+        (x, y): (i32, i32),
+        creature_map: &HashMap<i32, Creature>,
+        large: bool,
+    ) -> HashSet<i32> {
         let radius = if large { 2000 } else { 800 };
         creature_map
             .iter()
             .filter_map(|(&k, v)| {
                 let fx = v.x + v.vx;
                 let fy = v.y + v.vy;
-                let dist = (fx - self.x) * (fx - self.x) + (fy - self.y) * (fy - self.y);
+                let dist = (fx - x) * (fx - x) + (fy - y) * (fy - y);
                 if dist <= radius * radius {
                     Some(k)
                 } else {
@@ -65,6 +106,53 @@ impl Drone {
                 }
             })
             .collect()
+    }
+
+    fn get_scan(&self, creature_map: &HashMap<i32, Creature>, large: bool) -> HashSet<i32> {
+        Drone::get_scan_from_point((self.x, self.y), creature_map, large)
+    }
+
+    fn get_position_after_instruction(&self, i: &Instruction) -> (i32, i32) {
+        match i {
+            Instruction::Wait(_l) => (self.x, self.y + 300),
+            Instruction::Move(x, y, _l) => {
+                let vect = ((*x as i32) - self.x, (*y as i32) - self.y);
+                let d_sq = vect.1 * vect.1 + vect.0 * vect.0;
+                if d_sq <= 600 * 600 {
+                    return (*x as i32, *y as i32);
+                }
+                let l2 = (d_sq as f64).sqrt();
+                let scaled_vec = (
+                    (vect.0 as f64) / (l2 * 600.0),
+                    (vect.1 as f64) / (l2 * 600.0),
+                );
+                (
+                    (*x as i32) + (scaled_vec.0 as i32),
+                    (*y as i32) + (scaled_vec.1 as i32),
+                )
+            }
+        }
+    }
+
+    fn get_reachable_fish<'a>(
+        &self,
+        creature_map: &'a HashMap<i32, Creature>,
+        large: bool,
+    ) -> impl Iterator<Item = &'a Creature> {
+        let radius = if large { 2000 } else { 800 } + 600;
+        // Copy out of &self so that &self's lifetime isn't needed in the return type.
+        let sx = self.x;
+        let sy = self.y;
+        creature_map.values().filter_map(move |v| {
+            let fx = v.x + v.vx;
+            let fy = v.y + v.vy;
+            let dist = (fx - sx) * (fx - sx) + (fy - sy) * (fy - sy);
+            if dist <= radius * radius {
+                Some(v)
+            } else {
+                None
+            }
+        })
     }
 }
 
@@ -81,35 +169,39 @@ impl Player {
         creature: &Creature,
         creature_map: &HashMap<i32, Creature>,
         foe: &Self,
+        already_scanned: &HashSet<i32>,
     ) -> i32 {
         let type_base_score = creature.c_type + 1;
-        let has_first_creature_bonus = self.scanned_creatures.contains(&creature.id)
-            || foe.scanned_creatures.contains(&creature.id);
+        let has_first_creature_bonus = !self.scanned_creatures.contains(&creature.id)
+            && !foe.scanned_creatures.contains(&creature.id)
+            && !already_scanned.contains(&creature.id);
 
         let has_all_of_color = creature_map.iter().all(|(&k, v)| {
             if v.color != creature.color {
-                return false;
+                return true;
             }
             k == creature.id || self.scanned_creatures.contains(&k)
-        });
+        }) && !self.scanned_creatures.contains(&creature.id)
+            && !already_scanned.contains(&creature.id);
         let foe_has_all_of_color = creature_map.iter().all(|(&k, v)| {
             if v.color != creature.color {
-                return false;
+                return true;
             }
-            self.scanned_creatures.contains(&k)
+            foe.scanned_creatures.contains(&k)
         });
 
         let has_all_of_type = creature_map.iter().all(|(&k, v)| {
             if v.c_type != creature.c_type {
-                return false;
+                return true;
             }
-            k == creature.id || self.scanned_creatures.contains(&k)
-        });
+            k == creature.id || self.scanned_creatures.contains(&k) || already_scanned.contains(&k)
+        }) && !self.scanned_creatures.contains(&creature.id)
+            && !already_scanned.contains(&creature.id);
         let foe_has_all_of_type = creature_map.iter().all(|(&k, v)| {
             if v.c_type != creature.c_type {
-                return false;
+                return true;
             }
-            self.scanned_creatures.contains(&k)
+            foe.scanned_creatures.contains(&k)
         });
 
         let base = type_base_score * (if has_first_creature_bonus { 2 } else { 1 });
@@ -127,8 +219,13 @@ impl Player {
         foe: &Self,
     ) -> i32 {
         scan.iter()
-            .map(|&c| self.score_one_creature(creature_map.get(&c).unwrap(), creature_map, foe))
-            .sum()
+            .fold((HashSet::new(), 0), |(mut ss, total), &c| {
+                let addend =
+                    self.score_one_creature(creature_map.get(&c).unwrap(), creature_map, foe, &ss);
+                ss.insert(c);
+                (ss, total + addend)
+            })
+            .1
     }
 
     fn get_closest_drone_dist(&self, creature: &Creature) -> i32 {
@@ -141,6 +238,50 @@ impl Player {
             .unwrap()
     }
 
+    // TODO: we actually will have to worry about allocating drones.
+    fn best_move_greedy_fish(
+        &self,
+        creature_map: &HashMap<i32, Creature>,
+        foe: &Self,
+    ) -> Instruction {
+        creature_map
+            .values()
+            .max_by_key(|c| self.score_one_creature(c, creature_map, foe, &HashSet::default()))
+            .and_then(|c| {
+                Some(Instruction::Move(
+                    (c.x + c.vx).try_into().unwrap(),
+                    (c.x + c.vy).try_into().unwrap(),
+                    false,
+                ))
+            })
+            .unwrap()
+    }
+
+    fn best_move_greedy_reachable_fish(
+        &self,
+        drone_id: i32,
+        creature_map: &HashMap<i32, Creature>,
+        foe: &Self,
+        large_scan: bool,
+    ) -> Option<Instruction> {
+        self.drones
+            .get(&drone_id)
+            .unwrap()
+            .get_reachable_fish(creature_map, large_scan)
+            .max_by_key(|v| self.score_one_creature(v, creature_map, foe, &HashSet::default()))
+            .and_then(|c| {
+                Some(Instruction::Move(
+                    (c.x + c.vx).try_into().unwrap(),
+                    (c.x + c.vy).try_into().unwrap(),
+                    large_scan,
+                ))
+            })
+    }
+
+    // There are some flaws with this as a heuristic:
+    // 1) in general the centriod is meaningless -- it doesn't mean you can actually scan any fish.
+    // 2) adding the filter for nearby fish causes a lot of flip-flops in the center point without
+    //    the drone actually reaching the fish.
     fn best_move_centroid(
         &self,
         drone_id: i32,
@@ -164,7 +305,7 @@ impl Player {
             .map(|(&k, v)| {
                 let my_dist = furthest_fish - drone_fish_map.get(&k).unwrap();
                 let foe_dist = foe.get_closest_drone_dist(v);
-                let score = self.score_one_creature(v, creature_map, foe);
+                let score = self.score_one_creature(v, creature_map, foe, &HashSet::new());
                 (k, score * 1000 + my_dist + foe_dist)
             })
             .collect::<HashMap<i32, i32>>();
@@ -185,6 +326,53 @@ impl Player {
             })
             .fold((0., 0.), |(tx, ty), (nx, ny)| (tx + nx, ty + ny));
         Instruction::Move(px as u32, py as u32, false)
+    }
+
+    fn instruct_drone(
+        &self,
+        drone_id: usize,
+        creature_map: &HashMap<i32, Creature>,
+        foe: &Self,
+    ) -> Instruction {
+        // TODO: understand what order the game actually expects drone instructions and what
+        // order it provides the drone states in.
+        let (drone_key, drone) = self.drones.iter().next().unwrap();
+        let best_move = self
+            .best_move_greedy_fish(&creature_map, &foe)
+            .flip_light_for_drone_greedily(drone, creature_map, self, foe);
+        if let Some(greedy_move) = self
+            .best_move_greedy_reachable_fish(*drone_key, &creature_map, &foe, false)
+            .or_else(|| {
+                if drone.battery < 5 {
+                    None
+                } else {
+                    self.best_move_greedy_reachable_fish(*drone_key, &creature_map, &foe, true)
+                }
+            })
+        {
+            let expected_greedy_score = self.score_scan(
+                Drone::get_scan_from_point(
+                    drone.get_position_after_instruction(&greedy_move),
+                    creature_map,
+                    greedy_move.is_large_scan(),
+                ),
+                creature_map,
+                foe,
+            );
+            let expected_fish_score = self.score_scan(
+                Drone::get_scan_from_point(
+                    drone.get_position_after_instruction(&best_move),
+                    creature_map,
+                    best_move.is_large_scan(),
+                ),
+                creature_map,
+                foe,
+            );
+            if expected_greedy_score > expected_fish_score {
+                return greedy_move;
+            }
+        }
+        best_move
     }
 }
 
@@ -315,31 +503,28 @@ fn main() {
         for i in 0..my_drone_count as usize {
             // Write an action using println!("message...");
             // To debug: eprintln!("Debug message...");
-            // TODO: understand what order the game actually expects drone instructions and what
-            // order it provides the drone states in.
-            let drone = me
-                .drones
-                .values()
-                .next()
-                .unwrap();
-            let reachable_creatures = drone.get_scan(&creature_map, false);
-            let mut use_big_light = false;
-            if reachable_creatures.is_empty() && drone.battery > 10 {
-                let mut use_big_light = true;
-                let reachable_creatures = drone.get_scan(&creature_map, false);
-            }
-            me.best_move_centroid(
-                *me.drones.keys().next().unwrap(),
-                &creature_map,
-                &foe,
-                if reachable_creatures.is_empty() {
-                    None
-                } else {
-                    Some(reachable_creatures)
-                },
-            )
-            .with_big_light(use_big_light)
-            .print_instruction();
+            // Bad old centroid ideas
+            //            let reachable_creatures = drone.get_scan(&creature_map, false);
+            //            let mut use_big_light = false;
+            //            if reachable_creatures.is_empty() && drone.battery > 10 {
+            //                use_big_light = true;
+            //                reachable_creatures = drone.get_scan(&creature_map, true);
+            //            }
+            //            me.best_move_centroid(
+            //                *me.drones.keys().next().unwrap(),
+            //                &creature_map,
+            //                &foe,
+            //                if reachable_creatures.is_empty() {
+            //                    None
+            //                } else {
+            //                    Some(reachable_creatures)
+            //                },
+            //            )
+            //            .with_big_light(use_big_light)
+            //            .print_instruction();
+
+            me.instruct_drone(i, &creature_map, &foe)
+                .print_instruction();
         }
     }
 }
