356a4c9a6b3d3249be890a873edbb1a13b9010f3
Heman Gandhi <hemangandhi@gmail.com>
2024-02-25 14:44:51 +0900

    Divide lean book file into sections and insert chapter 4 exerciese

--- lean/book.lean
+++ lean/book.lean
@@ -1,4 +1,5 @@
-/- Test? -/
+
+section chapter_2_experiment
 
 /- α is implicit now so this lets it mostly hide. -/
 variable {α : Type}
@@ -9,3 +10,56 @@ def doTwice (f : α → α) := doOnceMore f f
 
 #check @doOnceMore
 #check doTwice
+
+end chapter_2_experiment
+
+section chapter_4_exercises
+
+section constructive
+variable (p q r : Prop)
+
+-- Double-negation implies LEM (from the text)
+example: ( ¬ ¬ p → p) → (p ∨ ¬ p) := sorry
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := sorry
+example : p ∨ q ↔ q ∨ p := sorry
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := sorry
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := sorry
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := sorry
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := sorry
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := sorry
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := sorry
+example : ¬p ∨ ¬q → ¬(p ∧ q) := sorry
+example : ¬(p ∧ ¬p) := sorry
+example : p ∧ ¬q → ¬(p → q) := sorry
+example : ¬p → (p → q) := sorry
+example : (¬p ∨ q) → (p → q) := sorry
+example : p ∨ False ↔ p := sorry
+example : p ∧ False ↔ False := sorry
+example : (p → q) → (¬q → ¬p) := sorry
+
+end constructive
+section non_constructive
+
+open Classical
+
+variable (p q r : Prop)
+
+example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := sorry
+example : ¬(p ∧ q) → ¬p ∨ ¬q := sorry
+example : ¬(p → q) → p ∧ ¬q := sorry
+example : (p → q) → (¬p ∨ q) := sorry
+example : (¬q → ¬p) → (p → q) := sorry
+example : p ∨ ¬p := sorry
+example : (((p → q) → p) → p) := sorry
+
+end non_constructive
+end chapter_4_exercises
