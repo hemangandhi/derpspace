a445d928d62281e4b72cc08dce4af9a1342f76f0
Heman Gandhi <hemangandhi@gmail.com>
2024-05-31 23:06:39 +0900

    Recategorize, and add a prompting system

--- pythons/algebra/factorial_nums.py
+++ pythons/algebra/factorial_nums.py
@@ -1,6 +1,6 @@
 # Being even more interested in https://xkcd.com/2835/
 
-from typing import Protocol, TypeVar, Optional, Union
+from typing import Protocol, TypeVar, Optional, Callable
 from collections.abc import Iterable
 import functools
 
@@ -40,7 +40,7 @@ class FactorialInt:
         self.digits = digits
         self.digit_representation = representation
 
-    @cached_property
+    @functools.cached_property
     def permutation(self):
         indices = list(range(len(self.digits) + 1))
         for digit, cycle_size in zip(self.digits[::-1], range(2, len(self.digits) + 2)):
@@ -49,15 +49,15 @@ class FactorialInt:
         return indices
 
     @classmethod
-    def from_int(cls, num: int, representation: DigitConverter[D]) -> FactorialInt[D]:
+    def from_int(cls, num: int, representation: DigitConverter[D] = UseDecimalWithinSlots()) -> 'FactorialInt[D]':
         digits = []
         while num > 0:
             next_digit = representation.to_digit(num % (len(digits) + 2))
             if next_digit is None:
-                raise ValueError(f"{num % (len(digits) + 2) is too large to be represented.")
-            num = num // len(digits) + 2
+                raise ValueError(f"{num % (len(digits) + 2)} is too large to be represented.")
+            num = num // (len(digits) + 2)
             digits.append(next_digit)
-         cls(digits[::-1], representation)
+        return cls(digits[::-1], representation)
 
     def __int__(self) -> int:
         acc = 0
@@ -78,3 +78,47 @@ class FactorialInt:
         if len(items) < len(self.digits):
             return [items[i] for i in self.permutation[:len(items)]]
         return [items[i] for i in self.permutation] + items[len(self.digits):]
+
+    def __str__(self) -> str:
+        return f"{self.digits} ({int(self)})"
+
+
+def ensure_prompt(prompt: str, validate: Callable[[str], Optional[str]]):
+    response = input(prompt)
+    while (error := validate(response)) is not None:
+        print(error)
+        response = input(prompt)
+    return response
+
+
+def run_prompt():
+    state: dict[int, FactorialInt[int]] = {}
+    ctr: int = 0
+
+    def check_command(command: str) -> Optional[str]:
+        fn, arg_str = command[0], command[1:]
+        if fn not in ['!', 'S', 'q']:
+            return f"Cannot handle function {fn}"
+        if fn == 'q':
+            return None
+        if not arg_str.isdigit():
+            return f"Cannot handle arg {arg_str}"
+        arg = int(arg_str)
+        if fn == '!':
+            state[ctr] = FactorialInt.from_int(arg)
+            return None
+        perm = state.get(arg)
+        if perm is None:
+            keys = ", ".join(map(str, state.keys()))
+            return f"Value {arg} has not been computed, try one of {keys}"
+        print(f"Permutation of {perm} is {perm.permutation}")
+        state[ctr] = perm
+
+    print("! = int->factorial base, S = get permutation of nth, q = quit")
+    while ensure_prompt(f"Enter command {ctr} > ", check_command) != "q":
+        ctr += 1
+        print("! = int->factorial base, S = get permutation of nth, q = quit")
+
+
+if __name__ == "__main__":
+    run_prompt()
