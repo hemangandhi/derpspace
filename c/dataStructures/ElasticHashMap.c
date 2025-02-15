6cd47298871fb9279193a7ba5a9a129d1ebeabc9
Heman Gandhi <hemangandhi@gmail.com>
2025-02-15 23:09:56 +0900

    Rename: camel-case functions, suffix _ for 'private'

--- c/dataStructures/ElasticHashMap.c
+++ c/dataStructures/ElasticHashMap.c
@@ -1,6 +1,8 @@
 #include "ElasticHashMap.h"
 
-static unsigned short _index_of_highest_bit(unsigned short n) {
+#include <stdlib.h>
+
+static unsigned short IndexOfHighestBit_(unsigned short n) {
     if (n <= 2) return n;
     unsigned short acc = 1;
     if ((n >> 8) > 0) {
@@ -22,9 +24,29 @@ static unsigned short _index_of_highest_bit(unsigned short n) {
     return acc;
 }
 
-static unsigned long int _phi(unsigned short i, unsigned short j) {
-    unsigned short top_j_bit = _index_of_highest_bit(j);
-    unsigned short top_i_bit = _index_of_highest_bit(i);
+// Cries in optimizing without templates: C++ or Rust (or probably Zig) could just
+// know the sizeof(n) at compile-time and stamp out pretty little loops per type.
+static unsigned long int IndexOfHighestBitLong_(unsigned long int n) {
+    if (n <= 2) return n;
+    unsigned long int acc = 1;
+    for (unsigned char offset = 32; offset > 1; offset >>= 1) {
+        if ((n >> offset) > 0) {
+            acc += offset;
+            n >>= offset;
+        }
+    }
+    if ((n >> 1) > 0) {
+        acc += 1;
+        n >>= 1;
+    }
+    return acc;
+}
+
+
+
+static unsigned long int Phi_(unsigned short i, unsigned short j) {
+    unsigned short top_j_bit = IndexOfHighestBit_(j);
+    unsigned short top_i_bit = IndexOfHighestBit_(i);
     // NOTE: ~0 extends?
     unsigned short j_ones = 0xFFFF >> (8 * sizeof(unsigned short) - top_j_bit);
     unsigned long int result = ((unsigned long) j_ones) << top_j_bit;
@@ -46,15 +68,23 @@ static unsigned long int _phi(unsigned short i, unsigned short j) {
 int main(int argc, char** argv) {
 #ifdef TEST_INDEX_OF_BIT
     printf("sizeof(unsigned short) = %lx\n", sizeof(unsigned short));
-    printf("Testing highest bit.\n");
-    for(unsigned int i = 0; i < 16; i++) {
-        printf("Highest bit of %u is %u.\n", i * 32, _index_of_highest_bit(i * 32));
+    printf("sizeof(unsigned long int) = %lx\n", sizeof(unsigned long int));
+    printf("Testing highest bit: shorts.\n");
+    for(unsigned short i = 0; i < 16; i++) {
+        printf("Highest bit of %u is %u.\n", i * 32, IndexOfHighestBit_(i * 32));
+    }
+    printf("Testing highest bit: long.\n");
+    for(unsigned long int i = 0; i < 16; i++) {
+        printf("Highest bit of %lu is %lu.\n", i * 32, IndexOfHighestBitLong_(i * 32));
+        printf("Highest bit of %lu is %lu.\n", i * 3002, IndexOfHighestBitLong_(i * 3002));
     }
 #endif
+#ifdef TEST_PHI_FUNCTION
     for (unsigned short i = 0; i < 3; i++) {
         for (unsigned short j = 0; j < 3; j++) {
-            printf("phi(%x, %x) = %lx\n", i, j, _phi(i, j));
+            printf("phi(%x, %x) = %lx\n", i, j, Phi_(i, j));
         }
     }
+#endif
 }
 #endif
