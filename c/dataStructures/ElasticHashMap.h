a04afb983b91011636b13144f1c94670949cc388
Heman Gandhi <hemangandhi@gmail.com>
2025-02-15 23:11:19 +0900

    Complete the API up to insertion

--- c/dataStructures/ElasticHashMap.h
+++ c/dataStructures/ElasticHashMap.h
@@ -11,9 +11,26 @@ typedef struct {
 
     // n, and which of the A_n we're allowed to probe until.
     unsigned long int capacity;
-    unsigned int batch_;
+    // Batch endpoint will be a running total while the length gives the size of the latest batch.
+    unsigned long int batch_endpoint_, next_batch_length_;
+
+    unsigned long int * subarray_loads_;
 
     void ** map_;
 } ElasticHashMap;
+
+// Allocates an ElasticHashMap.
+// TODO: make a version that only allocates the sub arrays?
+ElasticHashMap * ElasticMap_Initialize(HashFn hash, EqFn eq, unsigned long int capacity);
+
+void ElasticMap_Free(ElasticHashMap* map);
+
+typedef enum InsertionStatus {
+    ElasticMap_InsertionStatus_INSERTED,
+    ElasticMap_InsertionStatus_EQUAL_ELEMENT_FOUND,
+    ElasticMap_InsertionStatus_FULL,
+} ElasticMap_InsertionStatus;
+
+ElasticMap_InsertionStatus ElasticMap_Insert(ElasticHashMap* map, void* key);
 
 #endif
