9e515f954a5ed5629bf8af04eed77398442a66af
Heman Gandhi <hemangandhi@gmail.com>
2025-05-25 00:52:23 +0900

    Start on a header

---
+++ c++/van_emde_boas_et_al/include/van_emde_boas_et_al/van_emde_boas.h
@@ -0,0 +1,22 @@
+#include <concepts>
+#include <vector>
+
+namespace van_emde_boas_et_al {
+namespace van_emde_boas {
+
+template <std::integral K, typename V>
+class VanEmdeBoasTree {
+
+public:
+
+private:
+    K min, max;
+
+    struct Node {
+
+    };
+
+};
+
+}  // namespace van_emde_boas
+}  // namespace van_emde_boas_et_al
