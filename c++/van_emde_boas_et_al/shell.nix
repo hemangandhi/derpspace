c2175eef1ded444ab1531277e85ece066e1ac41f
Heman Gandhi <hemangandhi@gmail.com>
2025-05-24 15:15:09 +0900

    Init project boilerplate

---
+++ c++/van_emde_boas_et_al/shell.nix
@@ -0,0 +1,44 @@
+let
+  nixpkgs = import <nixpkgs> {};
+  cppFlags = [
+    "-std=c++17" # Or c++20, c++23 etc.
+    "-Wall"
+    "-Wextra"
+    "-pedantic"
+  ];
+  nvim = import ../../nvim.nix ( rec {
+    nixPkgs = nixpkgs;
+    neovim = nixpkgs.neovim;
+    customPackages = with nixpkgs.vimPlugins; {
+      start = [
+        coc-nvim
+        coc-git
+        vim-lsp
+        vim-nix
+        nvim-treesitter
+        vim-clang-format
+      ];
+    };
+  }); in
+nixpkgs.mkShell {
+  buildInputs = with nixpkgs; [
+    # Build system
+    gcc
+    cmake
+    # Testing framework
+    gtest
+    # Debugger
+    # gdb
+    # Editor
+    nvim
+    # coc needs this
+    nixpkgs.nodejs
+  ];
+
+  # Environment variables for your shell
+  shellHook = ''
+    export CXXFLAGS="${nixpkgs.lib.concatStringsSep " " cppFlags}"
+    export CFLAGS="${nixpkgs.lib.concatStringsSep " " cppFlags}"
+    echo "Use 'cmake .' to configure, 'cmake --build .' to build, and 'ctest' to run tests."
+  '';
+}
