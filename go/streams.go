0611a22ef87cca827cb342ad294455b70276f3f0
Heman Gandhi <hemangandhi@gmail.com>
2025-07-05 16:52:27 +0900

    Simplify stream map

--- go/streams.go
+++ go/streams.go
@@ -7,6 +7,8 @@ type Stream[T any] struct {
 	next  func() *Stream[T]
 }
 
+// Maps the fn over all the streams provided, returns a stream of the output.
+// The returned stream ends when any input stream ends.
 func streamMap[T any](fn func(...T) T, str ...Stream[T]) *Stream[T] {
 	getNexts := func(ss []Stream[T]) *[]Stream[T] {
 		sts := []Stream[T]{}
@@ -28,20 +30,17 @@ func streamMap[T any](fn func(...T) T, str ...Stream[T]) *Stream[T] {
 		return sts
 	}
 
-	nextStream := func() *Stream[T] {
-		val := fn(getValues(str)...)
-		composition := func() *Stream[T] {
-			nexts := getNexts(str)
-			if nexts == nil {
-				return nil
-			}
-			return streamMap(fn, *nexts...)
+	val := fn(getValues(str)...)
+	composition := func() *Stream[T] {
+		nexts := getNexts(str)
+		if nexts == nil {
+			return nil
 		}
-
-		return &Stream[T]{val, composition}
+		return streamMap(fn, *nexts...)
 	}
 
-	return nextStream()
+	return &Stream[T]{val, composition}
+
 }
 
 func main() {
