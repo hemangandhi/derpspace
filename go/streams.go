aa14de95c87022623eb30d819e6382f89bdc914b
Heman Gandhi <hemangandhi@gmail.com>
2025-07-05 13:24:38 +0900

    Fix format

--- go/streams.go
+++ go/streams.go
@@ -5,41 +5,41 @@ import "fmt"
 type VoidPtr interface{}
 
 type Stream struct {
-    value VoidPtr
-    next func() Stream
+	value VoidPtr
+	next  func() Stream
 }
 
-func streamMap (fn func(...VoidPtr) VoidPtr, str ...Stream) {
-    getNexts := func (ss ...Stream) []Stream {
-        sts := []Stream()
-        for _, s := range ss {
-            append(sts, s.next());
-        }
-        return sts;
-    };
+func streamMap(fn func(...VoidPtr) VoidPtr, str ...Stream) {
+	getNexts := func(ss ...Stream) []Stream {
+		sts := []Stream()
+		for _, s := range ss {
+			append(sts, s.next())
+		}
+		return sts
+	}
 
-    getValues := func (ss ...Stream) []VoidPtr {
-        sts := []Stream()
-        for _, s := range ss {
-            append(sts, s.value);
-        }
-        return sts;
-    };
+	getValues := func(ss ...Stream) []VoidPtr {
+		sts := []Stream()
+		for _, s := range ss {
+			append(sts, s.value)
+		}
+		return sts
+	}
 
-    nextStream := func () Stream {
-        val := fn(getValues(str)...);
-        composition = func () Stream {
-            return streamMap(fn, getNexts(str)...);
-        }
+	nextStream := func() Stream {
+		val := fn(getValues(str)...)
+		composition = func() Stream {
+			return streamMap(fn, getNexts(str)...)
+		}
 
-        return Stream{val, composition};
-    }
+		return Stream{val, composition}
+	}
 
-    nextStream();
+	nextStream()
 }
 
-func main () {
-    ones = Stream{1, func () {
-        return ones;
-    }};
+func main() {
+	ones = Stream{1, func() {
+		return ones
+	}}
 }
